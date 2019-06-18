d94264576ea7fa03e7e149fba41bc0198cef7278
a78275 <a78275@alunos.uminho.com>
2019-06-18 21:31:29 +0100

    artifacts

--- projeto/src/web/ConsultarPetsittersServlet.java
+++ projeto/src/web/ConsultarPetsittersServlet.java
@@ -1,5 +1,34 @@
 package web;
-import javax.servlet.http.HttpServlet;
 
+import beans.FacadeBeans;
+import com.google.gson.Gson;
+import main.Petsitter;
+import main.Util;
+import org.orm.PersistentSession;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet(name = "ConsultarPetsitters", urlPatterns = {"/ConsultarPetsitters"})
 public class ConsultarPetsittersServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        PersistentSession session = Util.getSession(request);
+        PrintWriter out = response.getWriter();
+
+        List<Petsitter> ps = FacadeBeans.consultarPetsitters(null, session);
+
+        String json = new Gson().toJson(ps);
+
+        out.print(json);
+        out.flush();
+    }
 }
