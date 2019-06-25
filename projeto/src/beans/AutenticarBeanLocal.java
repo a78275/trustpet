1bc66fe71afbfd74a53db671c7f0c6a1dbe17ffe
Cláudia Correia <a77431@alunos.uminho.pt>
2019-06-25 03:56:33 +0100

    Conflitos

---
+++ projeto/src/beans/AutenticarBeanLocal.java
@@ -0,0 +1,16 @@
+package beans;
+
+import org.orm.PersistentSession;
+
+import javax.ejb.Local;
+
+@Local
+public interface AutenticarBeanLocal {
+
+	/**
+	 * 
+	 * @param email
+	 * @param password
+	 */
+	boolean autenticar(String email, String password, PersistentSession session);
+}
