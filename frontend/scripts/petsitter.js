8e5e60e2cc70b8548d0e360f00fb34bdef2ecd04
a78275 <a78275@alunos.uminho.com>
2019-06-29 04:03:04 +0100

    edição de serviços e horario incompleta

--- frontend/scripts/petsitter.js
+++ frontend/scripts/petsitter.js
@@ -68,8 +68,15 @@ var vm = new Vue({
         servicos: [],
         horario: {},
         reviews: [],
+        servicos: [{ 'id': '1', 'designacao': 'Tomar conta' }, { 'id': '2', 'designacao': 'Levar à rua' }, { 'id': '3', 'designacao': 'Alimentar' }, { 'id': '4', 'designacao': 'Dar banho' }],
         tiposAnimal: [{ 'id': '1', 'tipo': 'Gato', 'img': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSf5fBKv_d93io82eZokxqn_4jsHFSolEpiqeNPxjxy12DIiM0T' }, { 'id': '2', 'tipo': 'Cão', 'img': 'https://essencecuidados.com.br/wp-content/uploads/2016/10/dog.jpg' }, { 'id': '3', 'tipo': 'Pássaro', 'img': 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/04/10/19/pinyon-jay-bird.jpg' }, { 'id': '4', 'tipo': 'Tartaruga', 'img': 'https://oliveridleyproject.org/wp-content/uploads/2018/05/Olive-ridley-turtle-baby-patient-Luna-recovering-rescue-centre-ORP.jpg' }],
-        tiposAnimaisSelecionados: []
+        tiposAnimaisSelecionados: [],
+        preco1: "",
+        preco2: "",
+        preco3: "",
+        preco4: "",
+        horario: [],
+        novoHorario: []
     },
     mounted: function () {
         if (localStorage.sucesso == "login") {
@@ -248,5 +255,75 @@ var vm = new Vue({
                 }
             }
         },
+        editarServicos: async function () {
+            var servicosSelecionados = []
+            if (this.preco1 != '') {
+                var s = '1:' + this.preco1
+                servicosSelecionados.push(s)
+            }
+            if (this.preco2 != '') {
+                s = '2:' + this.preco2
+                servicosSelecionados.push(s)
+            }
+            if (this.preco3 != '') {
+                s = '3:' + this.preco3
+                servicosSelecionados.push(s)
+            }
+            if (this.preco4 != '') {
+                s = '4:' + this.preco4
+                servicosSelecionados.push(s)
+            }
+
+            if (servicosSelecionados.length == 0) {
+                this.snackbar("Preencha pelo menos um serviço.")
+            }
+            else {
+                const response = await fetch("http://localhost:8080/trustpet_war_exploded/EditarServicos", {
+                    headers: {
+                        'Content-Type': 'application/json; charset=utf-8',
+                        'Token': localStorage.token
+                    },
+                    method: "POST",
+                    body: JSON.stringify({
+                        servicos: servicosSelecionados
+                    })
+                })
+                const content = await response.json()
+                console.log(JSON.stringify(content))
+                if (content.success) {
+                    window.location.replace("http://localhost/perfilPetsitter.html")
+                }
+                else {
+                    this.snackbar("Ocorreu um erro. Tente novamente.")
+                }
+            }
+        },
+        editarHorario: async function () {
+            if (this.horario.length == 0) {
+                this.snackbar("Selecione pelo menos uma hora.")
+            }
+            else {
+                const response = await fetch("http://localhost:8080/trustpet_war_exploded/EditarHorario", {
+                    headers: {
+                        'Content-Type': 'application/json; charset=utf-8',
+                        'Token': localStorage.token
+                    },
+                    method: "POST",
+                    body: JSON.stringify({
+                        horario: this.novoHorario
+                    })
+                })
+                const content = await response.json()
+                console.log(JSON.stringify(content))
+                if (content.success) {
+                    window.location.replace("http://localhost/perfilPetsitter.html")
+                }
+                else {
+                    // TODO: PÔR A DAR
+                    this.snackbar("Ocorreu um erro. Tente novamente.")
+                    window.location.replace("http://localhost/editarHorario.html")
+                }
+            }
+        }
     }
 })
